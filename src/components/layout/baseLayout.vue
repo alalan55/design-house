1336c1b2a8921acb2c4648b7fb22229290693c52
alalan55 <alla.ao4975@gmail.com>
2022-10-08 16:33:45 -0300

    Start creations of navbar

--- src/components/layout/baseLayout.vue
+++ src/components/layout/baseLayout.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { RouterView } from "vue-router";
+import AppBar from "@/components/organisms/appBar.vue";
 </script>
 
 <template>
   <div class="base-layout">
-    <header class="base-layout__header"></header>
+    <header class="base-layout__header">
+        <AppBar/>
+    </header>
     <div class="base-layout__body">
       <RouterView />
     </div>
---
+++ src/components/organisms/appBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nav-container">
+    <nav>barra de navegação</nav>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+@import "@/assets/index.scss";
+
+.nav-container {
+//   border: 2px solid red;
+  height: $dh-nav-container-height;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  nav {
+    border: 2px solid red;
+    width: 95%;
+    height: $dh-nav-height;
+  }
+}
+</style>
