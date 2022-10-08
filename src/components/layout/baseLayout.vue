f3edec9285d02aea4346f8deb5150d8e0894904b
alalan55 <alla.ao4975@gmail.com>
2022-10-08 16:20:09 -0300

    Start project

---
+++ src/components/layout/baseLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="base-layout">
+    <header class="base-layout__header"></header>
+    <div class="base-layout__body">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main>
+    <span> Home </span>
+  </main>
+</template>
