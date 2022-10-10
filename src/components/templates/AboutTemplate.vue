836171e7288d95348bfa2d983a47806c98416482
alalan55 <alla.ao4975@gmail.com>
2022-10-10 20:28:52 -0300

    Creation of about page

--- src/components/templates/AboutTemplate.vue
+++ src/components/templates/AboutTemplate.vue
@@ -1,20 +1,92 @@
 <script setup></script>
 
 <template>
-  <div class="about-template">Página de sobre</div>
+  <div class="about-template">
+    <div class="about-template__title">
+      <h1>Sobre nós.</h1>
+    </div>
+    <div class="about-template__infos">
+      <div class="left">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem eveniet
+          laudantium rem dignissimos quasi earum, consequuntur at mollitia explicabo enim
+          nisi provident doloribus neque consectetur saepe repellendus possimus esse iste!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quis, consequuntur
+          magni fugiat, aliquam esse deleniti consequatur tenetur a velit ipsam asperiores
+          harum libero officiis inventore laboriosam! Labore, sint iusto?
+        </p>
+      </div>
+      <div class="right">
+        <figure>
+          <img src="/img/about.jpg" alt="About" />
+        </figure>
+      </div>
+    </div>
+  </div>
 </template>
-
-<script setup></script>
 
 <style lang="scss" scoped>
 @import "@/assets/index.scss";
 
 .about-template {
-  min-height: calc(100vh - $dh-footer-heigth);
   @include maxPageSize;
+  min-height: calc(80vh - $dh-footer-heigth);
+  padding: $dh-top-containers 0 0 0;
+  color: $dh-black-1;
+  &__title {
+    @include transition;
+    @media (max-width: $dh-mobile) {
+      text-align: center;
+    }
+  }
+  &__infos {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: $dh-s-1;
+    & > div {
+      flex: 1 1 500px;
+    }
+
+    .right {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      figure {
+        width: 500px;
+        img {
+          border-radius: 50px;
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          @include transition;
+        }
+
+        @media (max-width: $dh-mobile) {
+          width: 100%;
+
+          img {
+            border-radius: 30px;
+          }
+        }
+      }
+    }
+    .left {
+      p {
+        letter-spacing: 0.3px;
+      }
+
+      @media (max-width: $dh-mobile) {
+        text-align: center;
+      }
+    }
+  }
 
   @media (min-width: 1600px) {
     @include maxPageSizeLargeScreen;
+  }
+  @media (max-width: $dh-mobile) {
+    padding: $dh-nav-container-height-mobile $dh-s-1 $dh-s-1;
   }
 }
 </style>
